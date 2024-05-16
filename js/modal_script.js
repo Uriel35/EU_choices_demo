31472228521cd0e404e839ba961ded2f9c8e9910
Uriel35 <uriespada@gmail.com>
2024-05-15 21:33:32 -0300

    demo

--- js/modal_script.js
+++ js/modal_script.js
@@ -4,7 +4,7 @@ function defineModal(openButton=undefined, modal, ctn, closeButton=undefined) {
         document.body.classList.add("modal-open"); // Para que no se scrollee la pagina cuando esta abierto un modal.
     })
     modal.addEventListener('click', (e) => {
-        if (e.target.contains(ctn)) {
+        if (e.target.contains(modal)) {
             modal.classList.remove('flex-active')
             document.body.classList.remove("modal-open");
         }
@@ -23,6 +23,7 @@ function defineModal(openButton=undefined, modal, ctn, closeButton=undefined) {
     }
 }
 
+defineModal(document.getElementById('hsmlp-ctn'), document.getElementById('hsmlp-modal'), document.getElementById('hsmlp-ctn'), document.getElementById('close-hsmlp-modal'))
 defineModal(document.getElementById('readme-button'), document.getElementById('readme-modal'), document.getElementById('readme-ctn'), document.getElementById('close-readme-modal'))
 defineModal(document.getElementById('statistics-button'), document.getElementById('statistics-modal'), document.getElementById('statistics-ctn'), document.getElementById('close-statistics-modal'))
 defineModal(undefined, document.getElementById('error-modal'), document.getElementById('error-modal-ctn'))
@@ -30,5 +31,22 @@ defineModal(undefined, document.getElementById('error-modal'), document.getEleme
 defineModal(document.getElementById('show-circle-modal-button'), document.getElementById('circle-modal'), document.getElementById('circles-modal-ctn'), document.getElementById('close-circles-modal'))
 defineModal(document.getElementById('report-question-button'), document.getElementById('report-question-modal'), document.getElementById('report-question-modal-ctn'), document.getElementById('close-report-question-modal'))
 
+
+const hsmlpCtn = document.getElementById("hsmlp-ctn")
+function vibrate(element){
+  setTimeout(function() {
+    element.classList.add("vibrate");
+  }, 100);
+  
+  setTimeout(function() {
+    element.classList.remove("vibrate");
+  }, 2000);
+}
+
+vibrate(hsmlpCtn)
+
+setInterval(function() {
+  vibrate(hsmlpCtn);
+}, 120000); // 120 seg (2 minutos)
 
 export default {defineModal}
